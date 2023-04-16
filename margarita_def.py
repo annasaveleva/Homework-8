f23082f4005fc0b2b88d51eba51efc6a3843e93f
Margarita Fadeeva <morganGTO@yandex.ru>
2023-04-17 00:27:18 +0300

    structured the project, added members and their function definitions

---
+++ margarita_def.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def do_margarita_code():
+    st.divider()
+    st.subheader("Задание")
+    st.info("Вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен/нет")
+    st.subheader("Решение")
+    st.text("И тут такая выводится программа Риты")
+    st.divider()
