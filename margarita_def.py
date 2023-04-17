798aa166a9d87300c21967de768ef8e2de4e0082
Margarita Fadeeva <morganGTO@yandex.ru>
2023-04-17 23:50:35 +0300

    Used pandas

--- margarita_def.py
+++ margarita_def.py
@@ -1,51 +1,16 @@
 import streamlit as st
+import pandas as pd
 
 def do_margarita_code():
-    st.divider()
     st.subheader("Задание")
     st.info("Вывести данные пассажиров с билетом нулевой стоимости, выбрав спасен/нет")
     st.subheader("Решение")
 
     save = "1" if st.checkbox("Выжил") else "0"
 
-    titanic_save = {}
-    titanic_not_save = {}
-
-    with open("data.csv") as data_file:
-        next(data_file)
-        for line in data_file:
-
-            lst = line.split(',')
-
-            passenger_id = lst[0]
-            survived = lst[1]
-            p_class = lst[2]
-            name = lst[3] + lst[4]
-            sex = lst[5]
-            age = lst[6]
-            sib_sp = lst[7]
-            parch = lst[8]
-            ticket = lst[9]
-            fare = lst[10]
-            cabin = lst[11]
-            embarked = lst[12]
-
-            if fare == '0' and survived == "1":
-                person_info_save = {"passengerId": passenger_id, "survived": survived, "pclass": p_class, "sex": sex,
-                               "age": age, "sibSp": sib_sp, "parch": parch, "ticket": ticket, "fare": fare, "cabin": cabin,
-                               "embarked": embarked}
-                titanic_save[name[1: -1]] = person_info_save
-
-            elif fare == '0' and survived == "0":
-                person_info_died = {"passengerId": passenger_id, "survived": survived, "pclass": p_class, "sex": sex,
-                               "age": age, "sibSp": sib_sp, "parch": parch, "ticket": ticket, "fare": fare, "cabin": cabin,
-                               "embarked": embarked}
-                titanic_not_save[name[1: -1]] = person_info_died
-    data_file.close()
+    df = pd.read_csv("data.csv")
 
     if save == "1":
-        st.dataframe(titanic_save)
+        st.write(df.loc[(df['Survived'] == 1) & (df['Fare'] == 0)])
     else:
-        st.dataframe(titanic_not_save)
-
-    st.divider()
+        st.write(df.loc[(df['Survived'] == 0) & (df['Fare'] == 0)])
