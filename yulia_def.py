204d068f46f118caa54d2c3378ec05f391ab1e58
Julia <yulia.schew4uck@yandex.ru>
2023-04-19 23:23:12 +0300

    Added def do_yulia_code

--- yulia_def.py
+++ yulia_def.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 def do_yulia_code():
-    st.text("Найти средний возраст пассажиров по каждому указанному классу обслуживания (Pclass)")
-    st.text("И тут такая выводится программа Юли")
+    st.subheader("Задание")
+    st.info("Найти средний возраст пассажиров по каждому указанному классу обслуживания (Pclass)")
+    st.subheader("Решение")
+    st.write("---")
+    data = pd.read_csv('data.csv')
+# Создаем список классов обслуживания
+    class_list = data['Pclass'].unique()
+# Создаем выпадающее меню для выбора класса обслуживания
+    selected_class = st.selectbox('Выберите класс обслуживания пассажиров:', class_list)
+# Фильтруем данные по выбранному классу и находим средний возраст
+    filtered_data = data[data['Pclass'] == selected_class]
+    mean_age = filtered_data['Age'].mean()
+# Выводим данные в приложение по выбору класса обслуживания пассажиров
+    st.write(f'Средний возраст пассажиров в классе {selected_class} равен {mean_age:.2f} лет.')
