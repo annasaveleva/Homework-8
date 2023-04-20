22c4180057b482af0d38b22993b0342fd8791eb2
Saveleva Anna <alessandarais1100@gmail.com>
2023-04-20 23:01:11 +0300

    Added anna_def.py

--- anna_def.py
+++ anna_def.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 def do_anna_code():
-    st.text("Найти количество мужчин и женщин по указанному классу обслуживания")
-    st.text("И тут такая выводится программа Ани")
+    st.title('Home Work 9')
+    st.subheader('Задание:')
+    st.subheader('Найти количество мужчин и женщин по указанному классу обслуживания')
+    st.subheader('Решение:')
+    df = pd.read_csv('data.csv')
+    pclass = df['Pclass'].unique()
+    selected_class = st.selectbox('Выберите класс обслуживания пассажиров:', pclass)
+    filtered_df = df[df['Pclass'] == selected_class]
+    count_male = (filtered_df[filtered_df['Sex'] == "male"]).shape[0]
+    count_female = (filtered_df[filtered_df['Sex'] == "female"]).shape[0]
+    st.text(f'Количество мужчин и женщин: Class {selected_class}. men: {count_male}, women: {count_female}')
