84aa4a64220c67cf9a46a0e220c333787f2158ee
kat-1003 <128515400+kat-1003@users.noreply.github.com>
2023-04-21 12:40:53 +0300

    Update kate_def.py
    попытка #1

--- kate_def.py
+++ kate_def.py
@@ -1,5 +1,18 @@
 import streamlit as st
-
+st.text("Посчитать количество пассажиров (выбрав пол, и спасенных или погибших)")
 def do_kate_code():
-    st.text("Посчитать количество пассажиров (выбрав пол, и спасенных или погибших)")
-    st.text("И тут такая выводится программа Кати")
+st.title('пассажиры Титаника')
+if st.chekbox("Спасен?"):
+   save = '1'
+else:
+   save = '0'
+with open("data.csv") as titanic_file:
+    count_male = 0
+    count_female = 0
+    str = line.split(',')[5]
+    for line in titanic_file:
+       if str = "sex"
+	   continue
+	    if str == 'male'
+	       count_male += 1
+st.text(f'количество мужчин' {count_male})
