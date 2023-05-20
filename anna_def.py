bd362ea62cdeb27bfd44e777ee1b1bd8948b9422
Saveleva Anna <alessandarais1100@gmail.com>
2023-05-20 13:33:01 +0300

    Added test_anna_def.py and changed anna_def.py

--- anna_def.py
+++ anna_def.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+
+def count_men_women_of_selected_class(df, selected_class):
+    filtered_df = df[df['Pclass'] == selected_class]
+    count_male = (filtered_df[filtered_df['Sex'] == "male"]).shape[0]
+    count_female = (filtered_df[filtered_df['Sex'] == "female"]).shape[0]
+    return count_male, count_female
+
+
 def do_anna_code():
     st.title('Home Work 9')
     st.subheader('Задание:')
@@ -9,7 +17,14 @@ def do_anna_code():
     df = pd.read_csv('data.csv')
     pclass = df['Pclass'].unique()
     selected_class = st.selectbox('Выберите класс обслуживания пассажиров:', pclass)
-    filtered_df = df[df['Pclass'] == selected_class]
-    count_male = (filtered_df[filtered_df['Sex'] == "male"]).shape[0]
-    count_female = (filtered_df[filtered_df['Sex'] == "female"]).shape[0]
+    count_men_women_of_selected_class(df, selected_class)
+    count_male, count_female = count_men_women_of_selected_class(df, selected_class)
+
     st.text(f'Количество мужчин и женщин: Class {selected_class}. men: {count_male}, women: {count_female}')
+
+
+do_anna_code()
+
+
+
+
