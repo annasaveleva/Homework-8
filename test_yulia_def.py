7d2d15d33c539816ecd3e7fd4e204e20e471401f
FadeevaMB <fadeevamb@kadastr.ru>
2023-05-23 13:52:46 +0300

    Added blank lines

--- test_yulia_def.py
+++ test_yulia_def.py
@@ -1,6 +1,7 @@
 from yulia_def import mean_age_for_class
 import pandas as pd
 
+
 def test_mean_age_for_class_countig():
     grid = {'Pclass': [1, 2, 3, 1, 2, 3], 'Age': [10, 0, 0, 15, 0, 0]}
     test_data = pd.DataFrame(grid)
