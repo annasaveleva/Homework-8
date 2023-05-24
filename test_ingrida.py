4608f60120a748dd238d9696d251c3e241f81309
Margarita Fadeeva <morganGTO@yandex.ru>
2023-05-24 18:47:19 +0300

    Added blanс line

--- test_ingrida.py
+++ test_ingrida.py
@@ -18,4 +18,3 @@ def test_mean_age_for_class_if_age_is_empty():
     grid = {'Pclass': [1, 2, 3, 1, 2, 3], 'Age': [10, 0, 0, None, 0, 0]}
     test_data = pd.DataFrame(grid)
     assert mean_age_for_class(test_data, 1) == 10
-    
