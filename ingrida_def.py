28c9bb0967b45605c55fc4e478ff689362d110ec
Margarita Fadeeva <morganGTO@yandex.ru>
2023-05-24 18:35:49 +0300

    Added tests

--- ingrida_def.py
+++ ingrida_def.py
@@ -1,20 +1,19 @@
 import streamlit as st
+import pandas as pd
+
+
+def mean_age_for_class(data, selected_class):
+    filtered_data = data[data['Pclass'] == selected_class]
+    return filtered_data['Age'].mean()
+
 
 def do_ingrida_code():
-    import streamlit as st
-    import pandas as pd
-    st.title('"Титаник"')
-    st.write("Вариант №12 выполнила Сабаляускайте И.В.")
     st.subheader("Задание")
     st.info("Найти средний возраст пассажиров по каждому указанному классу обслуживания (Pclass)")
     st.subheader("Решение")
+    st.write("---")
     data = pd.read_csv('data.csv')
-# Создаем список классов обслуживания
     class_list = data['Pclass'].unique()
-# Создаем выпадающее меню для выбора класса обслуживания
     selected_class = st.selectbox('Выберите класс обслуживания пассажиров:', class_list)
-# Фильтруем данные по выбранному классу и находим средний возраст
-    filtered_data = data[data['Pclass'] == selected_class]
-    mean_age = filtered_data['Age'].mean()
-# Выводим данные в приложение по выбору класса обслуживания пассажиров
+    mean_age = mean_age_for_class(data, selected_class)
     st.write(f'Средний возраст пассажиров в классе {selected_class} равен {mean_age:.2f} лет.')
--- test_ingrida.py
+++ test_ingrida.py
@@ -1,14 +1,14 @@
-from yulia_def import mean_age_for_class
+from ingrida_def import mean_age_for_class
 import pandas as pd
 
 
 def test_mean_age_for_class_countig():
-    grid = {'Pclass': [1, 2, 3, 1, 2, 3], 'Age': [10, 0, 0, 15, 0, 0]}
+    grid = {'Pclass': [1, 2, 3, 1, 2, 3], 'Age': [10, 20, 30, 15, 20, 30]}
     test_data = pd.DataFrame(grid)
-    assert mean_age_for_class(test_data, 1) == 12.5
+    assert mean_age_for_class(test_data, 3) == 30
 
 
-def test_mean_age_for_class_if_class_is_empty():
+def test_mean_age_for_class_if_pclass_is_empty():
     grid = {'Pclass': [1, 2, 3, None, 2, 3], 'Age': [10, 0, 0, 15, 0, 0]}
     test_data = pd.DataFrame(grid)
     assert mean_age_for_class(test_data, 1) == 10
